b27cd49eeda37fc0af17d2671ad21586091c557e
hafsa parker <60313322+HafsaParker@users.noreply.github.com>
2023-06-20 13:01:07 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Importing Liberaries
+import json
+import string
+import random
+import nltk
+import numpy as num
+from nltk.stem import WordNetLemmatizer
+import tensorflow as tf
+from tensorflow import keras
+from keras import Sequential
+from keras.layers import Dense,Dropout
+
+nltk.download("punkt")
+nltk.download("wordnet")
+
+
+
