ffa07e259f5d330daf5e29154801605c2f405c69
hafsa parker <60313322+HafsaParker@users.noreply.github.com>
2023-06-20 13:49:14 +0500

    data processing

--- main.py
+++ main.py
@@ -13,5 +13,34 @@ from keras.layers import Dense,Dropout
 nltk.download("punkt")
 nltk.download("wordnet")
 
+# loading data 
+f = open('data.json')
+data  = json.load(f)
+
+# processing Data
+lm = WordNetLemmatizer()
+#lists
+ourClass = []
+newWords = []
+docX = []
+docY=[]
+
+for d in data["ourIntents"]:
+    for p in d["patterns"]:
+        ournewTkns = nltk.word_tokenize(p)
+        newWords.extend(ournewTkns)
+        docX.append(p)
+        docY.append(d["tag"])
+        print("ournewTkns",ournewTkns)
+        print("newWords",newWords)
+        print("docX",docX)
+        print("docY",docY)
+    if d["tag"] not in ourClass:
+        ourClass.append(d["tag"])
+
+newWords = [lm.lemmatize(word.lower()) for word in newWords if word not in string.punctuation]
+newWords = sorted(set(newWords))
+ourClasses = sorted(set(ourClass))
+
 
 
